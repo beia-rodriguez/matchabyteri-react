113036c3020a0925f4c6119350e9b190374c0d85
Lacaba-Bry <bryan.lacaba@cvsu.edu.ph>
2026-05-21 19:36:49 +0800

    Remove env and vite config from tracking

--- backend/config/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("MAIL_HOST", "smtp.gmail.com");
-define("MAIL_USERNAME", "matchabyteri@gmail.com");
-define("MAIL_PASSWORD", "rvidcwepcsvcjbus");
-define("MAIL_PORT", 587);
-
-define("MAIL_FROM", "matchabyteri@gmail.com");
-define("MAIL_FROM_NAME", "Matcha By Teri");
